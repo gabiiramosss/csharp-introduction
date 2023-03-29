7f15938072e663ae8cafa11fde7278172687a15e
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-03-29 10:59:35 -0300

    Adicionado o programa de calculo de idade

---
+++ 3-age-program/Program.cs
@@ -0,0 +1,12 @@
+﻿//Entrada
+Console.WriteLine($"How old are you? ");
+int age = int.Parse(Console.ReadLine());
+
+//Processamento 
+//Months: {age * 12}
+//Days: {age * 365}
+//Hours: {age * 365 * 24}
+//Minutes: {age * 365 * 24 * 60}
+
+//Saída
+Console.WriteLine($"You are {age * 12} months, or {age * 365} days, or {age * 365 * 24} hours or {age * 365 * 24 * 60} minutes old :)");
