cfbfddead09194d3b2901fa7c7c081c6805f5b53
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-03-29 11:00:48 -0300

    Atividade de média adicionada

---
+++ 4-atividade-c#-2903/Program.cs
@@ -0,0 +1,24 @@
+﻿//Entrada
+Console.WriteLine($"Digite seu nome: ");
+string nome = (Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota 1: ");
+float nota1 = float.Parse (Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota 2: ");
+float nota2 = float.Parse (Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota 3: ");
+float nota3 = float.Parse (Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota 4: ");
+float nota4 = float.Parse (Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota 5: ");
+float nota5 = float.Parse (Console.ReadLine());
+
+//Processamento
+float media = ((nota1 + nota2 + nota3 + nota4 + nota5) / 5);
+
+//Saída
+Console.WriteLine($"A média do aluno " + nome + " é " + media);
