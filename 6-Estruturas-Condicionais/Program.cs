239a51fc7fef043f6d53f0138773b0f793fae750
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-03-30 09:17:07 -0300

    Aula de estruturas condicionais adicionada

---
+++ 6-Estruturas-Condicionais/Program.cs
@@ -0,0 +1,67 @@
+﻿//CONDICIONAL SIMPLES
+//Executa uma ou mais ações se a condição for verdadeira, no caso, se o Sol for true
+
+//bool sol = true;
+
+//if (sol)
+//{
+    //Console.WriteLine($"Vou para a praia!");   
+//}
+
+//CONDICIONAL COMPOSTA
+
+// bool sol = true;
+
+// if (sol)
+// {
+//     Console.WriteLine($"Vou para a praia!");
+// }
+// else
+// {
+//     Console.WriteLine($"Vou é dormir zzzzz!");
+// }
+
+
+// bool sol = true;
+// bool folga = true;
+
+// if (sol && folga)
+// {
+//     Console.WriteLine($"Vou para a praia!");
+// }
+// else
+// {
+//     Console.WriteLine($"Vou é dormir zzzzz!");
+// }
+
+//OPERADOR TERNÁRIO
+//Só podem ser utilizadas duas condições. ? corresponde ao if enquanto : corresponde ao else.
+
+// int x = 2;
+// int y = 3;
+
+// string eIgual = (x == y) ? "São iguais" : "São diferentes";
+// Console.WriteLine(eIgual);
+
+//CONDICIONAL ENCADEADA
+
+//maior ou igual a 18 anos: pode entrar na festa
+//maior de 16 menor que 18: pode entrar com responsável
+//menor que 16: não pode entrar
+
+// int idade = 18;
+
+// if (idade >= 18)
+// {
+//     Console.WriteLine($"Pode entrar na festa!");
+// }
+// else if (idade >= 16)
+// {
+//     Console.WriteLine($"Pode entrar na festa com a mamãe!");
+// }
+// else
+// {
+//     Console.WriteLine($"Não pode entrar, proibido para criancinhas!");
+// }
+
+//A condicional encadeada só funciona pq estamos lidando com o mesmo dado
