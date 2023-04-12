89b456f506aab10792a38856fe32a7948360b879
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-12 10:55:39 -0300

    Aula de Funções adicionada

---
+++ 10-Funcoes/Program.cs
@@ -0,0 +1,93 @@
+﻿//Criar um método para SOMAR 2 números
+
+// static float Somar(float n1, float n2)
+// {
+//     return (n1 + n2);
+// }
+
+// Console.WriteLine($"Informe o primeiro número: ");
+// float primeiroNumero = float.Parse(Console.ReadLine());
+
+// Console.WriteLine($"Informe o segundo número: ");
+// float segundoNumero = float.Parse(Console.ReadLine());
+
+// Console.WriteLine($"A soma de {primeiroNumero} + {segundoNumero} é : {Somar(primeiroNumero,segundoNumero)}");
+
+//Criar os métodos para as DEMAIS OPERAÇÕES matemáticas (-, *, /)
+//Subtração
+// static float Subtrair(float n1, float n2)
+// {
+//     return (n1 - n2);
+// }
+
+// Console.WriteLine($"Informe o primeiro número: ");
+// float primeiroNumero = float.Parse(Console.ReadLine());
+
+// Console.WriteLine($"Informe o segundo número: ");
+// float segundoNumero = float.Parse(Console.ReadLine());
+
+// Console.WriteLine($"A subtração de {primeiroNumero} - {segundoNumero} é : {Subtrair(primeiroNumero,segundoNumero)}");
+
+//Multiplicação
+//static float Multiplicar(float n1, float n2)
+// {
+//     return (n1 * n2);
+// }
+
+// Console.WriteLine($"Informe o primeiro número: ");
+// float primeiroNumero = float.Parse(Console.ReadLine());
+
+// Console.WriteLine($"Informe o segundo número: ");
+// float segundoNumero = float.Parse(Console.ReadLine());
+
+// Console.WriteLine($"A multiplicação de {primeiroNumero} * {segundoNumero} é : {Multiplicar(primeiroNumero,segundoNumero)}");
+
+//Divisão
+// static float Dividir(float n1, float n2)
+//  {
+//      return (n1 / n2);
+//  }
+
+//  Console.WriteLine($"Informe o primeiro número: ");
+//  float primeiroNumero = float.Parse(Console.ReadLine());
+
+//  Console.WriteLine($"Informe o segundo número: ");
+//  float segundoNumero = float.Parse(Console.ReadLine());
+
+//  Console.WriteLine($"A divisão de {primeiroNumero} / {segundoNumero} é : {Dividir(primeiroNumero,segundoNumero)}");
+
+
+//Método para trazer a HORA DO SISTEMA
+// static int MostrarHora()
+// {
+//     return DateTime.Now.Hour;
+// }
+
+// Console.WriteLine($"Agora são {MostrarHora()} horas");
+
+//ou
+// static int MostrarHoras()
+// {
+//     return DateTime.Now.Hour;
+// }
+// static int MostrarMinutos()
+// {
+//     return DateTime.Now.Minute;
+// }
+
+// Console.WriteLine($"Agora são {MostrarHoras()}h{MostrarMinutos()}");
+
+//Método para criar BARRA DE CARREGAMENTO
+// static void BarraCarregamento(string texto, int quantidadePontinhos, int tempo)
+// {
+//     Console.Write(texto);
+
+//     for (var i = 0; i < quantidadePontinhos; i++)
+//     {
+//         Console.Write($".");
+//         Thread.Sleep(tempo);
+//     }
+// }
+
+// BarraCarregamento("Carregando", 8, 1000);
+
